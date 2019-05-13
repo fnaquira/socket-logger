e2bf600cad42314037f76fd17c394bcf19e5fd82
Favio <fnaquira@conflux-aqp.com>
2019-05-12 21:03:11 -0500

    servidor correcto

--- gui/src/utils/socket.js
+++ gui/src/utils/socket.js
@@ -1,5 +1,5 @@
 import openSocket from 'socket.io-client';
-const socket = openSocket('http://localhost:8000');
+const socket = openSocket('http://see.conflux.pe:8000');
 
 const subscribeToLog = cb => {
 	socket.on('log-read', line => cb(line));
